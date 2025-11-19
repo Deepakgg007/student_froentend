d1ba201396a2957c21a7010ab4acd14b0831fc6a
Deepakgg007 <deepakg08199@gmail.com>
2025-11-19 18:05:47 +0530

    new

--- src/services/api.js
+++ src/services/api.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// export const API_BASE_URL = 'http://localhost:8000';
+export const API_BASE_URL = 'https://krishik-abiuasd.in';
 // export const API_BASE_URL = 'http://192.168.1.9:8000';
-export const API_BASE_URL = 'http://16.16.76.74:8000';
+// export const API_BASE_URL = 'http://16.16.76.74:8000';
 
 const api = axios.create({
   baseURL: `${API_BASE_URL}/api`,
